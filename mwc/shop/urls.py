7a08c6ca01d3053a8386d28d98cd8d34da6877bf
imran-hosen <imranhosen.bsmrstu@gmail.com>
2023-08-02 11:34:20 +0600

    add-contect-me-page-(Importent)

--- mwc/shop/urls.py
+++ mwc/shop/urls.py
@@ -21,9 +21,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='AboutUs'),
-    path('contact', views.contact, name='ContactUs'),
+    path('contact/', views.contact, name='ContactUs'),
     path('tracker', views.tracker, name='TrackingStatus'),
-    path('productview', views.productView, name='Search'),
+    path('products<int:myid>', views.productView, name='Search'),
     path('checkout', views.checkout, name='Checkout')
 
 
